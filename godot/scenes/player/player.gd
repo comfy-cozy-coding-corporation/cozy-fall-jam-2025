f928e82c00527ad3dee5015275f9902ab6693aa8
lina-roether <lina.roether@proton.me>
2025-10-24 19:26:55 +0200

    feat(player): add gliding

--- godot/scenes/player/player.gd
+++ godot/scenes/player/player.gd
@@ -7,6 +7,9 @@ const JUMP_VELOCITY = 120
 const JUMP_MAX_HOLD_TIME = 0.5
 const JUMPING_GRAVITY_ACC = 100
 const FALLING_GRAVITY_ACC = 500
+const GLIDING_GRAVITY_ACC = 200
+const GLIDING_AIR_RESISTANCE = 2.0
+const GLIDING_FORWARD_ACC = 200
 
 const MIN_RUNNING_ANIM_SPEED = 0.5
 
@@ -15,7 +18,8 @@ enum State {
 	RUNNING,
 	JUMPING,
 	RISING,
-	FALLING
+	FALLING,
+	GLIDING
 }
 
 enum PlayerAnimation {
@@ -25,6 +29,7 @@ enum PlayerAnimation {
 	JUMPING,
 	RISE_INTO_FALLING,
 	STAND_INTO_FALLING,
+	FALL_INTO_GLIDING
 }
 
 var state = State.STANDING
@@ -57,6 +62,9 @@ func play(anim: PlayerAnimation):
 		PlayerAnimation.STAND_INTO_FALLING:
 			sprite.play("landing", -1, true)
 			animation_queue.push_back("falling")
+		PlayerAnimation.FALL_INTO_GLIDING:
+			sprite.play("fall_to_glide")
+			animation_queue.push_back("gliding")
 
 func change_state(new_state):
 	if new_state == state: return
@@ -64,7 +72,7 @@ func change_state(new_state):
 
 	match new_state:
 		State.STANDING:
-			if state == State.FALLING || state == State.RISING || state == State.JUMPING:
+			if state == State.FALLING || state == State.RISING || state == State.JUMPING || state == State.GLIDING:
 				play(PlayerAnimation.LAND_INTO_STANDING)
 			else:
 				play(PlayerAnimation.STANDING)
@@ -77,10 +85,15 @@ func change_state(new_state):
 				play(PlayerAnimation.RISE_INTO_FALLING)
 			else:
 				play(PlayerAnimation.STAND_INTO_FALLING)
+		State.GLIDING:
+			play(PlayerAnimation.FALL_INTO_GLIDING)
 	state = new_state
 
+var facing_dir = -1
+
 func sprite_face(direction):
-	sprite.flip_h = direction == 1
+	facing_dir = direction
+	sprite.flip_h = facing_dir == 1
 
 func sprite_set_running_speed(speed):
 	if state != State.RUNNING: return
@@ -107,9 +120,14 @@ func run_decelerate(delta: float):
 	velocity.x = max(0, abs(velocity.x) - delta_vel) * sign(velocity.x)
 	sprite_set_running_speed(abs(velocity.x))
 
+
+var jump_window_counter: int = 0
+
 func jump():
 	change_state(State.JUMPING)
+	jump_window_counter += 1
 	jump_input_window.start()
+
 	velocity.y -= JUMP_VELOCITY
 
 	if Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right"):
@@ -139,29 +157,56 @@ func process_on_ground(delta: float):
 	else:
 		change_state(State.STANDING)
 		run_decelerate(delta)
-	
+
+func apply_gravity(delta):
+	if state == State.JUMPING:
+		velocity.y += JUMPING_GRAVITY_ACC * delta
+	elif state == State.GLIDING:
+		velocity.y += GLIDING_GRAVITY_ACC * delta
+	else:
+		velocity.y += FALLING_GRAVITY_ACC * delta
+
+func apply_air_resistance(delta):
+	if sign(velocity.y) <= 0:
+		return
+	velocity.y *= (1 - clamp(GLIDING_AIR_RESISTANCE * delta, 0, 1))
+
 
 func process_in_air(delta: float):
-	if state == State.JUMPING && !Input.is_action_pressed("jump"):
+	if state == State.JUMPING && (jump_window_counter == 0 || !Input.is_action_pressed("jump")):
 		change_state(State.RISING)
 
 	if state == State.RISING && sign(velocity.y) != -1:
 		change_state(State.FALLING)
-	
-	if state == State.FALLING && is_on_floor():
+
+	if state != State.JUMPING && is_on_floor():
 		change_state(State.STANDING)
 		return
 	
-	if state == State.JUMPING:
-		velocity.y += JUMPING_GRAVITY_ACC * delta
-	else:
-		velocity.y += FALLING_GRAVITY_ACC * delta
+	if Input.is_action_just_pressed("jump"):
+		match state:
+			State.FALLING, State.RISING: change_state(State.GLIDING)
+			State.GLIDING: change_state(State.FALLING)
+
+	if state == State.GLIDING:
+		velocity.x += facing_dir * GLIDING_FORWARD_ACC * delta
+		if Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right"):
+			sprite_face(-1)
+		elif Input.is_action_pressed("move_right") && !Input.is_action_pressed("move_left"):
+			sprite_face(1)
+
+	apply_gravity(delta)
+
+	if state == State.GLIDING:
+		apply_air_resistance(delta)
+
+	
 
 func _physics_process(delta: float) -> void:
 	match state:
 		State.STANDING, State.RUNNING:
 			process_on_ground(delta)
-		State.JUMPING, State.RISING, State.FALLING:
+		State.JUMPING, State.RISING, State.FALLING, State.GLIDING:
 			process_in_air(delta)
 	move_and_slide()
 
@@ -170,4 +215,4 @@ func _ready() -> void:
 	sprite.animation_finished.connect(play_next_animation)
 	jump_input_window.wait_time = JUMP_MAX_HOLD_TIME
 	jump_input_window.one_shot = true
-	jump_input_window.timeout.connect(change_state.bind(State.RISING))
+	jump_input_window.timeout.connect(func(): jump_window_counter -= 1)
