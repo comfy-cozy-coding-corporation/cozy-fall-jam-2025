7cdf7feb904a24eb1175f8117d6c26c276583de7
lina-roether <lina.roether@proton.me>
2025-10-24 17:07:29 +0200

    feat(player): implement jumping and falling

--- godot/scenes/player/player.gd
+++ godot/scenes/player/player.gd
@@ -3,26 +3,74 @@ extends CharacterBody2D
 const TOP_SPEED = 200
 const ACCELERATION = 500
 const DECELERATION = 1500
+const JUMP_SPEED = 200
+const FALLING_GRAVITY_ACC = 500
 
 const MIN_RUNNING_ANIM_SPEED = 0.5
 
 enum State {
-	IDLE,
-	RUNNING
+	STANDING,
+	RUNNING,
+	JUMPING,
+	FALLING
 }
 
-var state = State.IDLE
+enum PlayerAnimation {
+	LAND_INTO_STANDING,
+	STANDING,
+	RUNNING,
+	JUMPING,
+	JUMP_INTO_FALLING,
+	STAND_INTO_FALLING,
+}
+
+var state = State.STANDING
 
 @onready var sprite: AnimatedSprite2D = self.find_child("PlayerSprite")
+
+var animation_queue = []
+
+func play_next_animation():
+	var next_anim = animation_queue.pop_front()
+	sprite.play(next_anim)
+
+func play(anim: PlayerAnimation):
+	animation_queue.clear()
+	match anim:
+		PlayerAnimation.LAND_INTO_STANDING:
+			sprite.play("landing")
+			animation_queue.push_back("standing")
+		PlayerAnimation.STANDING:
+			sprite.play("standing")
+		PlayerAnimation.RUNNING:
+			sprite.play("running")
+		PlayerAnimation.JUMPING:
+			sprite.play("jumping")
+		PlayerAnimation.JUMP_INTO_FALLING:
+			sprite.play("jump_to_fall")
+			animation_queue.push_back("falling")
+		PlayerAnimation.STAND_INTO_FALLING:
+			sprite.play("landing", -1, true)
+			animation_queue.push_back("falling")
 
 func change_state(new_state):
 	if new_state == state: return
 	sprite.speed_scale = 1
 	match new_state:
-		State.IDLE:
-			sprite.play("idle")
+		State.STANDING:
+			if state == State.FALLING or state == State.JUMPING:
+				play(PlayerAnimation.LAND_INTO_STANDING)
+			else:
+				play(PlayerAnimation.STANDING)
 		State.RUNNING:
-			sprite.play("running")
+			play(PlayerAnimation.RUNNING)
+		State.JUMPING:
+			play(PlayerAnimation.JUMPING)
+		State.FALLING:
+			if state == State.JUMPING:
+				play(PlayerAnimation.JUMP_INTO_FALLING)
+			else:
+				play(PlayerAnimation.STAND_INTO_FALLING)
 	state = new_state
 
 func sprite_face(direction):
@@ -53,16 +101,47 @@ func run_decelerate(delta: float):
 	velocity.x = max(0, abs(velocity.x) - delta_vel) * sign(velocity.x)
 	sprite_set_running_speed(abs(velocity.x))
 
+func jump():
+	velocity.y -= JUMP_SPEED
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("move_left"):
+func process_on_ground(delta: float):
+	if !is_on_floor():
+		change_state(State.FALLING)
+		return
+
+	if Input.is_action_pressed("jump"):
+		jump()
+		change_state(State.JUMPING)
+	elif Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right"):
 		change_state(State.RUNNING)
 		run_accelerate(-1, delta)
-	elif Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right") && !Input.is_action_pressed("move_left"):
 		change_state(State.RUNNING)
 		run_accelerate(1, delta)
 	else:
-		change_state(State.IDLE)
+		change_state(State.STANDING)
 		run_decelerate(delta)
+	
+
+func process_in_air(delta: float):
+	if state == State.FALLING && is_on_floor():
+		change_state(State.STANDING)
+		return
+	
+	if state == State.JUMPING && sign(velocity.y) != 1:
+		change_state(State.FALLING)
+
+	velocity.y += FALLING_GRAVITY_ACC * delta
+
+func _physics_process(delta: float) -> void:
+	print(state)
+	match state:
+		State.STANDING, State.RUNNING:
+			process_on_ground(delta)
+		State.JUMPING, State.FALLING:
+			process_in_air(delta)
 	move_and_slide()
 
+
+func _ready() -> void:
+	sprite.animation_finished.connect(play_next_animation)
