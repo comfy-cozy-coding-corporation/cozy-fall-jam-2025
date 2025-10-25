442a8abea8713c941e7b247ea642b1a73bf4e721
Lilith Roth <lilith@roth.systems>
2025-10-25 15:36:12 +0200

    feat: web export tuning & main menu

--- godot/scenes/main_menu_scene.gd
+++ godot/scenes/main_menu_scene.gd
@@ -4,7 +4,6 @@ extends Node2D
 @export var settings_scene:PackedScene
 
 @onready var overlay := %FadeOverlay
-@onready var continue_button := %ContinueButton
 @onready var new_game_button := %NewGameButton
 @onready var settings_button := %SettingsButton
 @onready var exit_button := %ExitButton
@@ -16,19 +15,13 @@ func _ready() -> void:
 	overlay.visible = true
 	new_game_button.disabled = game_scene == null
 	settings_button.disabled = settings_scene == null
-	continue_button.visible = SaveGame.has_save() and SaveGame.ENABLED
 	
 	# connect signals
 	new_game_button.pressed.connect(_on_play_button_pressed)
-	continue_button.pressed.connect(_on_continue_button_pressed)
 	settings_button.pressed.connect(_on_settings_button_pressed)
 	exit_button.pressed.connect(_on_exit_button_pressed)
 	overlay.on_complete_fade_out.connect(_on_fade_overlay_on_complete_fade_out)
-	
-	if continue_button.visible:
-		continue_button.grab_focus()
-	else:
-		new_game_button.grab_focus()
+	new_game_button.grab_focus()
 
 func _on_settings_button_pressed() -> void:
 	new_game = false
@@ -36,11 +29,6 @@ func _on_settings_button_pressed() -> void:
 	overlay.fade_out()
 	
 func _on_play_button_pressed() -> void:
-	next_scene = game_scene
-	overlay.fade_out()
-	
-func _on_continue_button_pressed() -> void:
-	new_game = false
 	next_scene = game_scene
 	overlay.fade_out()
 
