246a1909363f027876bb9af010ff9d8dc13943b4
lina-roether <lina.roether@proton.me>
2025-10-25 15:28:26 +0200

    feat(player): make constants exported!

--- godot/scenes/player/player.gd
+++ godot/scenes/player/player.gd
@@ -1,25 +1,37 @@
 extends CharacterBody2D
 
-const GLIDES_PER_JUMP = 1
-const MAX_RUNNING_SPEED = 200
-const MAX_FALL_CONTROL_SPEED = 100
-const MAX_GLIDING_SPEED = 300
-const RUNNING_FORWARD_ACCELERATION = 500
-const RUNNING_BACKWARD_ACCELERATION = 10000
-const RUNNING_DECELERATION = 1500
-const FALLING_ACCELERATION = 500
-const GLIDING_FORWARD_ACCELERATION = 300
-const GLIDING_BACKWARD_ACCELERATION = 900
-const JUMP_VELOCITY = 120
-const JUMP_MAX_HOLD_TIME = 0.5
-const FLAP_VELOCITY = 280
-const FLAP_FORWARD_VELOCITY = 100
-const JUMPING_GRAVITY = 100
-const FALLING_GRAVITY = 500
-const GLIDING_GRAVITY = 200
-const GLIDING_AIR_RESISTANCE = 2.0
+@export_group("Movement")
 
-const MIN_RUNNING_ANIM_SPEED = 0.5
+@export_subgroup("Running")
+@export var max_running_speed: float = 200
+@export var running_acceleration: float = 500
+@export var running_turnaround_acceleration: float = 10000
+@export var running_deceleration: float = 1500
+
+@export_subgroup("Jumping")
+@export var jump_velocity: float = 120
+@export var jumping_gravity: float = 100
+@export var jump_max_hold_time: float = 0.5
+
+@export_subgroup("Falling")
+@export var falling_gravity: float = 500
+@export var max_air_control_speed: float = 100
+@export var air_control_accceleration: float = 500
+
+@export_subgroup("Gliding")
+@export var glides_per_jump: int = 1
+@export var max_gliding_speed: float = 300
+@export var gliding_gravity: float = 200
+@export var gliding_air_resistance: float = 2.0
+@export var gliding_acceleration: float = 300
+@export var gliding_turnaround_acceleration: float = 900
+
+@export_subgroup("Flapping")
+@export var flap_velocity: float = 280
+@export var flap_forward_velocity: float = 100
+
+@export_group("Visual")
+@export var min_running_animation_speed: float = 0.5
 
 enum State {
 	STANDING,
@@ -105,32 +117,32 @@ func change_state(new_state):
 
 var facing_dir = -1
 
-func sprite_face(direction):
+func sprite_face(direction: float):
 	facing_dir = direction
 	sprite.flip_h = facing_dir == 1
 
-func sprite_set_running_speed(speed):
+func sprite_set_running_speed(speed: float):
 	if state != State.RUNNING: return
-	sprite.speed_scale = (speed / MAX_RUNNING_SPEED) * (1 - MIN_RUNNING_ANIM_SPEED) + MIN_RUNNING_ANIM_SPEED
+	sprite.speed_scale = (speed / max_running_speed) * (1 - min_running_animation_speed) + min_running_animation_speed
 
 
-func run_accelerate(direction, delta: float):
+func run_accelerate(direction: float, delta: float):
 	sprite_face(direction)
 
 	var delta_vel
 	if sign(velocity.x) == direction:
-		delta_vel = delta * direction * RUNNING_FORWARD_ACCELERATION
+		delta_vel = delta * direction * running_acceleration
 	else:
-		delta_vel = delta * direction * RUNNING_BACKWARD_ACCELERATION
+		delta_vel = delta * direction * running_turnaround_acceleration
 
-	if velocity.x * direction > MAX_RUNNING_SPEED:
+	if velocity.x * direction > max_running_speed:
 		return
 
-	velocity.x = min(MAX_RUNNING_SPEED, direction * (velocity.x + delta_vel)) * direction
+	velocity.x = min(max_running_speed, direction * (velocity.x + delta_vel)) * direction
 	sprite_set_running_speed(abs(velocity.x))
 
 func run_decelerate(delta: float):
-	var delta_vel = delta * RUNNING_DECELERATION
+	var delta_vel = delta * running_deceleration
 	velocity.x = max(0, abs(velocity.x) - delta_vel) * sign(velocity.x)
 	sprite_set_running_speed(abs(velocity.x))
 
@@ -144,16 +156,16 @@ func jump():
 	jump_window_counter += 1
 	jump_input_window.start()
 
-	glides_available = GLIDES_PER_JUMP
+	glides_available = glides_per_jump
 
-	velocity.y -= JUMP_VELOCITY
+	velocity.y -= jump_velocity
 
 	if Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right"):
 		sprite_face(-1)
-		velocity.x = -max(abs(velocity.x), MAX_RUNNING_SPEED)
+		velocity.x = -max(abs(velocity.x), max_running_speed)
 	elif Input.is_action_pressed("move_right") && !Input.is_action_pressed("move_left"):
 		sprite_face(1)
-		velocity.x = max(abs(velocity.x), MAX_RUNNING_SPEED)
+		velocity.x = max(abs(velocity.x), max_running_speed)
 	else:
 		velocity.x = 0
 
@@ -177,29 +189,29 @@ func process_on_ground(delta: float):
 
 func apply_gravity(delta):
 	if state == State.JUMPING:
-		velocity.y += JUMPING_GRAVITY * delta
+		velocity.y += jumping_gravity * delta
 	elif state == State.GLIDING:
-		velocity.y += GLIDING_GRAVITY * delta
+		velocity.y += gliding_gravity * delta
 	else:
-		velocity.y += FALLING_GRAVITY * delta
+		velocity.y += falling_gravity * delta
 
 func apply_air_resistance(delta):
 	if sign(velocity.y) <= 0:
 		return
-	velocity.y *= (1 - clamp(GLIDING_AIR_RESISTANCE * delta, 0, 1))
+	velocity.y *= (1 - clamp(gliding_air_resistance * delta, 0, 1))
 
 
 
 func flap():
-	if velocity.y > -FLAP_VELOCITY:
-		velocity.y = max(velocity.y, 0) - FLAP_VELOCITY
+	if velocity.y > -flap_velocity:
+		velocity.y = max(velocity.y, 0) - flap_velocity
 
 	if Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right"):
 		sprite_face(-1)
-		velocity.x = -max(-velocity.x, FLAP_FORWARD_VELOCITY)
+		velocity.x = -max(-velocity.x, flap_forward_velocity)
 	elif Input.is_action_pressed("move_right") && !Input.is_action_pressed("move_left"):
 		sprite_face(1)
-		velocity.x = max(velocity.x, FLAP_FORWARD_VELOCITY)
+		velocity.x = max(velocity.x, flap_forward_velocity)
 	else:
 		velocity.x = 0
 
@@ -209,19 +221,19 @@ func flap():
 func glide_accelerate(delta: float):
 	var delta_vel
 	if sign(velocity.x) == facing_dir:
-		delta_vel = GLIDING_FORWARD_ACCELERATION * delta * facing_dir
+		delta_vel = gliding_acceleration * delta * facing_dir
 	else:
-		delta_vel = GLIDING_BACKWARD_ACCELERATION * delta * facing_dir
+		delta_vel = gliding_turnaround_acceleration * delta * facing_dir
 
-	velocity.x = min(MAX_GLIDING_SPEED, facing_dir * (velocity.x + delta_vel)) * facing_dir
+	velocity.x = min(max_gliding_speed, facing_dir * (velocity.x + delta_vel)) * facing_dir
 	if Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right"):
 		sprite_face(-1)
 	elif Input.is_action_pressed("move_right") && !Input.is_action_pressed("move_left"):
 		sprite_face(1)
 
 func fall_accelerate(direction: float, delta: float):
-	var delta_vel = delta * direction * FALLING_ACCELERATION
-	velocity.x = min(MAX_RUNNING_SPEED, direction * (velocity.x + delta_vel)) * direction
+	var delta_vel = delta * direction * air_control_accceleration
+	velocity.x = min(max_running_speed, direction * (velocity.x + delta_vel)) * direction
 
 
 func process_in_air(delta: float):
@@ -270,6 +282,6 @@ func _physics_process(delta: float) -> void:
 
 func _ready() -> void:
 	sprite.animation_finished.connect(play_next_animation)
-	jump_input_window.wait_time = JUMP_MAX_HOLD_TIME
+	jump_input_window.wait_time = jump_max_hold_time
 	jump_input_window.one_shot = true
 	jump_input_window.timeout.connect(func(): jump_window_counter -= 1)
