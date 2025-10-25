717eed4038fbbd146705b2ee8fd3adca6edf575c
Lilith Roth <lilith@roth.systems>
2025-10-25 17:33:45 +0200

    feat: park bench obstacle

--- godot/scenes/environment/environment_manager.gd
+++ godot/scenes/environment/environment_manager.gd
@@ -1,24 +1,30 @@
 extends Node2D
 
 
-@export var bush_scene: PackedScene
-@export var tree_scene: PackedScene
 
+@export_group("Bush Configuration")
+@export var bush_scene: PackedScene
 @export var min_bushes: int = 3
 @export var max_bushes: int = 5
-@export var min_trees: int = 3
-@export var max_trees: int = 5
-
-# Defines the valid positions for a bush to spawn
-# bush_pos_area[0] represents the min. x & y coordinates
-# bush_pos_area[1] represents the max. x & y coordinates
 @export var bush_pos_area: CollisionShape2D
 @export var bush_min_distance: int = 0
+@export var bush_nodes_container: Node2D
+
+@export_group("Tree Configuration")
+@export var tree_scene: PackedScene
+@export var min_trees: int = 3
+@export var max_trees: int = 5
 @export var tree_pos_area: CollisionShape2D
 @export var tree_min_distance: int = 300
-
-@export var bush_nodes_container: Node2D
 @export var tree_nodes_container: Node2D
+
+@export_group("Bench Configuration")
+@export var bench_scene: PackedScene
+@export var min_benches: int = 2
+@export var max_benches: int = 2
+@export var bench_pos_area: CollisionShape2D
+@export var bench_min_distance: int = 300
+@export var bench_nodes_container: Node2D
 
 func _ready() -> void:
 	var amount_bushes = randi_range(min_bushes, max_bushes)
@@ -26,6 +32,9 @@ func _ready() -> void:
 	
 	var amount_trees = randi_range(min_trees, max_trees)
 	generate_random_structure(tree_scene, tree_pos_area, amount_trees, tree_min_distance, tree_nodes_container, "Tree-")
+	
+	var amount_benches = randi_range(min_benches, max_benches)
+	generate_random_structure(bench_scene, bench_pos_area, amount_benches, bench_min_distance, bench_nodes_container, "Bench-")
 	
 func generate_random_structure(
 	scene: PackedScene,
@@ -36,7 +45,12 @@ func generate_random_structure(
 	node_name: String,
 ) -> void:
 	var new_structure_positions: PackedVector2Array = [Vector2.ZERO]
+	
+	# Emergency exit if there's not enough space to distance out more objects just stop doing so
+	var placing_iterations: int = 0
 	for i in range(amount):
+		if placing_iterations == 25:
+			return
 		var new_structure_pos = Vector2(
 			randf_range(valid_positions.shape.get_rect().position.x, valid_positions.shape.get_rect().position.x + valid_positions.shape.get_rect().size.x), 
 			randf_range(valid_positions.shape.get_rect().position.y, valid_positions.shape.get_rect().position.y + valid_positions.shape.get_rect().size.y)
@@ -52,6 +66,7 @@ func generate_random_structure(
 					position_adjusted = true
 			if not position_adjusted:
 				break
+		placing_iterations += 1
 		var structure_instance: Node2D = scene.instantiate()
 		structure_instance.name = node_name + str(i)
 		parent_container.add_child(structure_instance)
